eba68045a0956fa81f4f00e0ce83dd9834d870f0
unknown <sbandy9210@gmail.com>
2022-03-24 10:08:15 -0500

    coasters

--- models/park.js
+++ models/park.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema
 
 const Park = new Schema(
     {
-        name:{ type: String, required: true},
+        parkName:{ type: String, required: true},
         description:{ type: String, required: false},
-        picture:[{ type: String, required: true}],
+        picture:[{ type: String, required: false}],
         location: {type : String, required: true},
         reviews:[{type: Schema.Types.ObjectId, ref: 'review'}],
         coaster:[{type: Schema.Types.ObjectId, ref: 'coaster'}]
